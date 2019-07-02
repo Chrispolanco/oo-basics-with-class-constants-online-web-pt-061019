c144d59ce38bd29f388993049ee3d61f9c6bdd34
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-02 04:49:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
+  array= []
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand 
+    array << brand 
+    
+    
   end
 
   def cobble
